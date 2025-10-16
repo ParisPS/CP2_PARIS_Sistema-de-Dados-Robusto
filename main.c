593f81de2e3955f526cf31d372e8cb77c2ec0690
ParisPS <68931223+ParisPS@users.noreply.github.com>
2025-10-16 09:47:46 -0300

    Update main.c

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ void Task1 (void *pv)
         }
  
         var->id = seq++;
-        var->random_number = seq+;
+        var->random_number = seq+1;
  
         if(xQueueSend(fila, &var, 0) != pdTRUE)
         {
@@ -172,5 +172,6 @@ void app_main(void)
     esp_task_wdt_add(hTask3);
 
 }
+
 
 
